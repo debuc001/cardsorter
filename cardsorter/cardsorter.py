c2279d47de173f4c9db8293423512b78b7ad737a
debuc001 <bennduko@googlemail.com>
2023-11-11 13:02:23 +0100

    1st servo test

--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -35,8 +35,8 @@ servo = AngularServo(21, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
     servo.angle = 90
-    sleep(2)
+    time.sleep(2)
     servo.angle = 0
-    sleep(2)
+    time.sleep(2)
     servo.angle = -90
-    sleep(2)
+    time.sleep(2)
