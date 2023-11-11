e1c4e8184d094b19fe373276acb506d3ab9d2536
debuc001 <bennduko@googlemail.com>
2023-11-11 11:38:36 +0100

    disabled pytesseract

--- cardsorter/image_recognition.py
+++ cardsorter/image_recognition.py
@@ -1,9 +1,9 @@
-import pytesseract
+#import pytesseract
 from PIL import Image
 
 def text_recognition(image):
  # Perform OCR using PyTesseract
- text = pytesseract.image_to_string(Image.open(image), timeout=2, lang="eng")
+ #text = pytesseract.image_to_string(Image.open(image), timeout=2, lang="eng")
 
  # Print the extracted text
  print(text)
