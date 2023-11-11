acbadc6762427a5983da7b61b1e87dcb361b94fb
debuc001 <bennduko@googlemail.com>
2023-11-11 14:08:28 +0100

    servo tests

--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -39,32 +39,25 @@ GPIO.setup(servo_pin, GPIO.OUT)
 pwm=GPIO.PWM(servo_pin, 50)
 pwm.start(0)
 
-def setAngle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(servo_pin, True)
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(1)
-    GPIO.output(servo_pin, False)
-    pwm.ChangeDutyCycle(duty)
-
-count = 0
-numLoops = 2
-
-while count < numLoops:
-    print("set to 0-deg")
-    setAngle(20)
-    time.sleep(1)
-
-        
-    print("set to 90-deg")
-    setAngle(90)
-    time.sleep(1)
-
-    print("set to 135-deg")
-    setAngle(160)
-    time.sleep(1)
-    
-    count=count+1
-
-pwm.stop()
-GPIO.cleanup()
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
+p.start(2.5) # Initialisierung
+try:
+  while True:
+    p.ChangeDutyCycle(2.5)
+    time.sleep(5)
+    print("2.5")
+    p.ChangeDutyCycle(5)
+    time.sleep(5)
+    print("5")
+    p.ChangeDutyCycle(7.5)
+    time.sleep(5)
+    print("7.5")
+    p.ChangeDutyCycle(10)
+    time.sleep(5)
+    print("10")
+    p.ChangeDutyCycle(12.5)
+    time.sleep(5)
+    print("12.5")
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
