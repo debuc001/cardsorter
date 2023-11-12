c614477088c70e5e4317980fd6d53058675368e8
debuc001 <bennduko@googlemail.com>
2023-11-12 23:35:31 +0100

    cleaned up dataframe wanted cards

--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -3,33 +3,30 @@ import requests
 import json
 import numpy as np
 import time
+import logging
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 
-from dataframe_functions import *
+from wanted_cards_dataframe import *
 from image_manipulation import *
 from image_recognition import *
-from motor_control import *
+#from motor_control import *
 
-deckstats_card_dataframe = pd.DataFrame( columns = ['QUANTITY','CARDNAME_EN'])
 scryfall_base_api_url = "https://api.scryfall.com/"
 scryfall_cardname_object = "cards/named?exact=!"
 image_input = "../tests/testcard.jpg"
 
-#Create dataframe 
-deckstats_card_dataframe = get_deckstats_card_dataframe(deckstats_card_dataframe)
-deckstats_card_dataframe = preprocessing_dataframe(deckstats_card_dataframe)
-
-#print(deckstats_card_dataframe)
+wanted_cards = WantedCards()
+wanted_cards.print_dataframe_wanted_cards()
 
 #crop_image(image_input, get_image_width(image_input) - 150, 100)
 #cropped_image = "../tests/testcard_cropped.jpg"
 #text_recognition(cropped_image)
 
-init_dc_motor()
-init_servo_motor()
-set_servo_motor_middle()
+#init_dc_motor()
+#init_servo_motor()
+#set_servo_motor_middle()
 
 
 #while True:
@@ -38,13 +35,19 @@ set_servo_motor_middle()
 # time.sleep(0.2)
 # set_servo_motor_middle()
 
-set_servo_motor_left()
-time.sleep(5)
-set_servo_motor_right()
-time.sleep(5)
-set_servo_motor_stop()
+#set_servo_motor_left()
+#time.sleep(5)
+#set_servo_motor_right()
+#time.sleep(5)
+#set_servo_motor_stop()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.IN)
-while True:
- print(GPIO.input(17))
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(17,GPIO.IN)
+#while True:
+# print(GPIO.input(17))
+
+
+
+logging.info('This message is just informative')
+logging.debug('My missions is to provide information about debugging.')
+logging.critical("A Critical Logging Message")
---
+++ cardsorter/wanted_cards_dataframe.py
@@ -0,0 +1,60 @@
+import numpy
+import pandas
+import requests
+
+class WantedCards:
+ def __init__(self):
+  self.deckstats_card_dataframe = pandas.DataFrame( columns = ['QUANTITY','CARDNAME_EN'])
+
+ #Fill dataframe from file resources/deckstats_cardlist.txt
+  with open("./deckstats_cardlist.txt") as file:
+   for item in file:
+    #Seperate quantity and cardnames, remove newline
+    line_from_deckstats_cardlist = item.split(sep=" ",maxsplit=1)
+    line_from_deckstats_cardlist = [r.strip() for r in line_from_deckstats_cardlist]
+    #Add each modified line to dataframe
+    self.deckstats_card_dataframe.loc[len(self.deckstats_card_dataframe)] = line_from_deckstats_cardlist
+
+  #Remove everything after // -> only one name of double sided card is left in dataframe
+  for ind in self.deckstats_card_dataframe.index:
+   cardname = self.deckstats_card_dataframe.iat[ind,1]
+   head, sep, tail = cardname.partition('//')
+   self.deckstats_card_dataframe.iat[ind,1] = head
+  #Remove everything after # -> only the name of the commander is left
+  for ind in self.deckstats_card_dataframe.index:
+   cardname = self.deckstats_card_dataframe.iat[ind,1]  
+   head, sep, tail = cardname.partition('#')
+   self.deckstats_card_dataframe.iat[ind,1] = head
+
+ def print_dataframe_wanted_cards(self):
+  print(self.deckstats_card_dataframe)
+
+  
+#
+#Add support for german cards.
+#This is not supported yet.
+#
+def add_row_for_german_cards(dataframe):
+ dataframe['CARDNAME_DE'] = numpy.nan
+
+def fill_dataframe_with_cardnames_de(dataframe, scryfall_base_api_url, scryfall_cardname_object):
+ for ind in dataframe.index:
+  #Get the english cardnames from dataframe
+  cardname = dataframe.iat[ind,1]
+  scryfall_request_cardname_api_url = scryfall_base_api_url + scryfall_cardname_object + "\"" + cardname + "\""
+  response_english_card = requests.get(scryfall_request_cardname_api_url)
+  #Report if request fails
+  if response_english_card.status_code != 200:
+   print(response_english_card.status_code)
+
+  #Build request for german cardname  
+  scryfall_card_json = response_english_card.json()
+  scryfall_set_cn_api_url = scryfall_base_api_url + "cards/" + scryfall_card_json['set'] + "/" +  scryfall_card_json['collector_number'] + "/de"
+  response_german_card = requests.get(scryfall_set_cn_api_url)
+  #Request the german card name. If no german equivalent is found, ignore the request.
+  try:
+   dataframe.iat[ind,2] = response_german_card.json()['printed_name']
+  except KeyError:
+   print("It seems that the card \"" + scryfall_card_json['name'] + "\" has no german equivalent")
+   print(scryfall_set_cn_api_url)
+ return dataframe
