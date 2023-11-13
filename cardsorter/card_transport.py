9b3a91c380e3579e520f15f33f51373418d78056
debuc001 <bennduko@googlemail.com>
2023-11-13 14:27:41 +0100

    transport test

---
+++ cardsorter/card_transport.py
@@ -0,0 +1,17 @@
+import time
+import RPi.GPIO as GPIO
+
+from motor_control import *
+
+light_sensor_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(light_sensor_pin,GPIO.IN)
+light_sensor = GPIO.input(light_sensor_pin)
+#while True:
+# print()
+
+def card_transport_next():
+ dc_motor_start()
+ while light_sensor:
+  print("TEST: light sensor true : " + light_sensor)
+ dc_motor_stop()
--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -5,19 +5,15 @@ import numpy as np
 import time
 import logging
 
-#import RPi.GPIO as GPIO
-
-
 from wanted_cards_dataframe import *
 from image_manipulation import *
 from image_recognition import *
 #from motor_control import *
+from card_transport import *
 
 #crop_image(image_input, get_image_width(image_input) - 150, 100)
 #cropped_image = "../tests/testcard_cropped.jpg"
 #text_recognition(cropped_image)
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(17,GPIO.IN)
-#while True:
-# print(GPIO.input(17))
+while True:
+ card_transport_next()
