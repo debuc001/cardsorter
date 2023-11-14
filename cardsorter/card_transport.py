021c1a4c9540363d12f97128a319add22b86ea7c
debuc001 <bennduko@googlemail.com>
2023-11-14 11:39:10 +0100

    motor tests

--- cardsorter/card_transport.py
+++ cardsorter/card_transport.py
@@ -10,18 +10,25 @@ light_sensor = GPIO.input(light_sensor_pin)
 #while True:
 # print()
 
+def light_sensor():
+ return GPIO.input(light_sensor_pin)
+
 def card_transport_next():
  #give next card and stop dc motor when lightsensor is blocked
+ stop_dc_motor = True
  dc_motor_start(50)
- while GPIO.input(light_sensor_pin):
-  dc_motor_stop()
+ while stop_dc_motor:
+  if light_sensor():
+   stop_dc_motor = False
+   dc_motor_stop()
+
  #decide if card is wanted or not
  #WIP: ONLY FOR TEST:
  card_is_wanted = True
- if card_is_wanted:
-  servo_motor_set_angle("left")
- else:
-  servo_motor_set_angle("right")
- sleep(2)
+  if card_is_wanted:
+   servo_motor_set_angle("left")
+  else:
+   servo_motor_set_angle("right")
+ time.sleep(2)
  servo_motor_set_angle("mid")
 
