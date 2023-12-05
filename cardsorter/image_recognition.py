3d13a552baa678f3356956b6e292f97b4ec62754
debuc001 <bennduko@googlemail.com>
2023-12-05 14:01:23 +0100

    first ir tests

--- cardsorter/image_recognition.py
+++ cardsorter/image_recognition.py
@@ -1,9 +1,16 @@
-#import pytesseract
-from PIL import Image
+from mtgscan.text import MagicRecognition
+from mtgscan.ocr.azure import Azure
+import os
 
-def text_recognition(image):
- # Perform OCR using PyTesseract
- #text = pytesseract.image_to_string(Image.open(image), timeout=2, lang="eng")
+def image_recognition():
+ os.environ['AZURE_VISION_ENDPOINT'] = 'https://mtgscan-cvr.cognitiveservices.azure.com/'
+ os.environ['AZURE_VISION_KEY'] = '1dee066d4686498596110672904ec81a'
+ 
+ azure = Azure()
+ rec = MagicRecognition(file_all_cards="all_cards.txt", file_keywords="Keywords.json")
 
- # Print the extracted text
- print(text)
+ def compare(image):
+  box_texts = azure.image_to_box_texts(image)
+  deck = rec.box_texts_to_deck(box_texts)
+  for c, k in deck:
+   print(c, k)
