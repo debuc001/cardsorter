aa8509b16a1b0286c7be6bc9e45ce53a332e23dc
debuc001 <bennduko@googlemail.com>
2023-12-06 16:58:48 +0100

    testing

--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -1,19 +1,19 @@
-from card_transport import *
-from image_capture import *
-from image_recognition import *
-from motor_control import *
+#from card_transport import *
+#from image_capture import *
+#from image_recognition import *
+#from motor_control import *
 from wanted_cards_dataframe import *
 
 wanted_cards = WantedCards()
-wanted_cards.print_dataframe_wanted_cards()
-camera = image_capture()
+#wanted_cards.print_dataframe_wanted_cards()
+#camera = image_capture()
 
 
 
-card_transport_next()
-camera.take_picture()
+#card_transport_next()
+#camera.take_picture()
 #compare cards
-#for ind in wanted_cards.index:
-# cardname = wanted_cards.iat[ind,1]
-# print(cardname)
-print(wanted_cards.return_wanted_cards)
+for ind in wanted_cards.deckstats_card_dataframe.index:
+ cardname = wanted_cards.deckstats_card_dataframe.iat[ind,1]
+ print(cardname)
+#print(wanted_cards.deckstats_card_dataframe)
