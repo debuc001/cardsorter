3f48ff32fab59d027640129ba36850104cd643fd
debuc001 <bennduko@googlemail.com>
2023-12-12 10:52:49 +0100

    tests

--- cardsorter/cardsorter.py
+++ cardsorter/cardsorter.py
@@ -3,18 +3,18 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 #from card_transport import *
-#from image_capture import *
+from image_capture import *
 from image_recognition import *
 #from motor_control import *
 from wanted_cards_dataframe import *
 
 wanted_cards = WantedCards()
-#camera = image_capture()
+camera = image_capture()
 img_recog = ImageRecognition()
 
 
 #card_transport_next()
-#camera.take_picture()
+camera.take_picture()
 #compare cards
 card_captured = img_recog.recognize()
 is_card_wanted = False
